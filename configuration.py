79ae9d67618d78b622edecac108180e78d9d4512
StephaniaLozano <soystefylozano@gmial.com>
2025-08-12 11:02:26 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-f6dee283-4a1c-4630-930a-eb991f24db45.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
